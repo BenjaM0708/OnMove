e2482194e96f58be623ce4258895f9c6ad3e7a64
Benja Muñoz <benjamunozja@gmail.com>
2026-06-04 19:56:12 +0200

    LocalHost Testing

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -4,4 +4,7 @@ import { Database } from '../types/types';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
+console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('VITE_SUPABASE_PUBLISHABLE_KEY:', import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
